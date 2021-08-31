df284fbd58c4899149f7b17cd5fac4824ee2b3a9
Nattaphoom Chaipreecha <attomos@gmail.com>
2021-08-31 15:24:59 +0700

    feat(cajal): add macros and nav layer

--- keyboards/walletburner/cajal/keymaps/attomos/keymap.c
+++ keyboards/walletburner/cajal/keymaps/attomos/keymap.c
@@ -10,12 +10,32 @@
 enum cajal_layers {
   _QWERTY,
   _ONE,
-  _TWO
+  _TWO,
+  _NAV
+};
+
+enum cajal_keycodes {
+  QWERTY = SAFE_RANGE,
+  ATOM_M1,
+  ATOM_M2,
+  ATOM_M3,
+  ATOM_M4,
+  ATOM_M5,
+  ATOM_M6,
+  ATOM_M7,
+  ATOM_M8,
+  ATOM_M9,
+  ATOM_N0,
+  ATOM_N1,
+  ATOM_S1,
+  ATOM_S2
 };
 
 
 // better spacebar
-#define AA_SPC LT(_ONE, KC_SPC)
+/* #define AA_SPC LT(_ONE, KC_SPC) */
+
+#define AA_SLSH LT(_TWO, KC_SLSH)
 
 // My custom Mod-Tap constants
 #define AA_Z MT(MOD_LCTL | MOD_LALT | MOD_LGUI | MOD_LSFT, KC_Z)
@@ -26,19 +46,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, RGB_TOG,
       LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
       KC_LSFT, AA_Z, AA_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP,
-      KC_LCTL, KC_LGUI, KC_LALT, AA_SPC, KC_SPC, KC_SPC, MO(_TWO), KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL, KC_LGUI, KC_LALT, MO(_ONE), KC_SPC, MO(_TWO), MO(_NAV), KC_LEFT, KC_DOWN, KC_RGHT
   ),
-  [_ONE] = LAYOUT_stagger(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_RBRC, KC_BSLS, RGB_MOD,
+  [_ONE] = LAYOUT_stagger(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MUTE, KC_TRNS, RGB_MOD,
       KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_QUOT, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_PGUP,
+      KC_TRNS, ATOM_M7, ATOM_M8, KC_TRNS, KC_TRNS, KC_TRNS, ATOM_M6, ATOM_M3, KC_TRNS, KC_PSCR, KC_TRNS, KC_PGUP,
       RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
   ),
   [_TWO] = LAYOUT_stagger(
-      KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SAD, RGB_SAI, KC_TRNS, RGB_VAI,
+      KC_TILD, ATOM_M1, KC_TRNS, ATOM_M2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_F13, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, ATOM_N0, ATOM_N1, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, RGB_SAD, RGB_SAI, KC_TRNS, RGB_VAI,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUD, RGB_VAD, RGB_HUI
   ),
+  [_NAV] = LAYOUT_stagger(
+      KC_TRNS, ATOM_M4, KC_TRNS, ATOM_M5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, ATOM_S1, ATOM_S2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
@@ -50,6 +76,114 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
     return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ATOM_M1:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGUP);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGUP);
+      }
+      return false;
+      break;
+    case ATOM_M2:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGDN);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGDN);
+      }
+      return false;
+      break;
+    case ATOM_M3:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        register_code(KC_F12);
+      } else {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_F12);
+      }
+      return false;
+      break;
+    case ATOM_M4:
+      if (record->event.pressed) {
+        SEND_STRING("[t");
+      }
+      return false;
+      break;
+    case ATOM_M5:
+      if (record->event.pressed) {
+        SEND_STRING("]t");
+      }
+      return false;
+      break;
+    case ATOM_M6:
+      if (record->event.pressed) {
+        SEND_STRING("]T");
+      }
+      return false;
+      break;
+    case ATOM_M7:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_4);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_4);
+      }
+      return false;
+      break;
+    case ATOM_M8:
+      if (record->event.pressed) {
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_3);
+      } else {
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_3);
+      }
+      return false;
+      break;
+    case ATOM_N0:
+      if (record->event.pressed) {
+        SEND_STRING("3j");
+      }
+      return false;
+      break;
+    case ATOM_N1:
+      if (record->event.pressed) {
+        SEND_STRING("3k");
+      }
+      return false;
+      break;
+    case ATOM_S1:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_1);
+      }
+      return false;
+      break;
+    case ATOM_S2:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_2);
+      }
+      return false;
+      break;
+  }
+  return true;
 }
 
 
