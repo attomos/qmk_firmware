d9762f748c8fe091b9b4206e2c9dc31abe353176
Nattaphoom Chaipreecha <attomos@gmail.com>
2021-08-31 23:39:19 +0700

    feat(cajal): add tmux macros and having fun with the encoder

--- keyboards/walletburner/cajal/keymaps/attomos/keymap.c
+++ keyboards/walletburner/cajal/keymaps/attomos/keymap.c
@@ -28,7 +28,11 @@ enum cajal_keycodes {
   ATOM_N0,
   ATOM_N1,
   ATOM_S1,
-  ATOM_S2
+  ATOM_S2,
+  TMUX_M1,
+  TMUX_M2,
+  TMUX_M3,
+  TMUX_M4
 };
 
 
@@ -46,24 +50,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, RGB_TOG,
       LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
       KC_LSFT, AA_Z, AA_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, AA_SLSH, KC_UP,
-      KC_LCTL, KC_LGUI, KC_LALT, MO(_ONE), KC_SPC, MO(_TWO), XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL, KC_LGUI, KC_LALT, MO(_ONE), KC_SPC, MO(_TWO), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_ONE] = LAYOUT_stagger(
       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MUTE, KC_TRNS, RGB_MOD,
-      KC_F13, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_QUOT,
+      KC_F13, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_QUOT, KC_TRNS,
       KC_TRNS, ATOM_M7, ATOM_M8, KC_TRNS, KC_TRNS, KC_TRNS, ATOM_M6, ATOM_M3, KC_TRNS, KC_PSCR, KC_TRNS, KC_PGUP,
       RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
   ),
   [_TWO] = LAYOUT_stagger(
-      KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, ATOM_N0, ATOM_N1, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, RGB_SAD, RGB_SAI, KC_TRNS, RGB_VAI,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUD, RGB_VAD, RGB_HUI
   ),
   [_NAV] = LAYOUT_stagger(
-      KC_TRNS, ATOM_M1, KC_TRNS, ATOM_M2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, ATOM_M1, KC_TRNS, ATOM_M2, TMUX_M3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, ATOM_S1, ATOM_S2, ATOM_M4, ATOM_M5, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TMUX_M4, KC_TRNS, TMUX_M1, TMUX_M2, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
@@ -90,15 +94,41 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
+// helper/macro functions
+
+void tmux_previous_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("9"));
+}
+
+void tmux_next_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("0"));
+}
+
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLD);
-        } else {
-            tap_code(KC_VOLU);
-        }
+  if (index == 0) {
+    if (IS_LAYER_ON(_QWERTY)) {
+      if (clockwise) {
+        tap_code(KC_VOLD);
+      } else {
+        tap_code(KC_VOLU);
+      }
+    } else if (IS_LAYER_ON(_ONE)) {
+      if (clockwise) {
+        tap_code(KC_BRID);
+      } else {
+        tap_code(KC_BRIU);
+      }
+    } else if (IS_LAYER_ON(_NAV)) {
+      if (clockwise) {
+        tmux_previous_session();
+      } else {
+        tmux_next_session();
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -174,6 +204,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code(KC_LALT);
         unregister_code(KC_LSFT);
         unregister_code(KC_3);
+      }
+      return false;
+      break;
+    case TMUX_M1:
+      if (record->event.pressed) {
+        tmux_previous_session();
+      }
+      return false;
+      break;
+    case TMUX_M2:
+      if (record->event.pressed) {
+        tmux_next_session();
+      }
+      return false;
+      break;
+    case TMUX_M3:
+      // tmux: rename session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT("4"));
+      }
+      return false;
+      break;
+    case TMUX_M4:
+      // tmux: start a new session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT(";"));
+        SEND_STRING("new");
+        register_code(KC_ENT);
+      } else {
+        unregister_code(KC_ENT);
       }
       return false;
       break;
