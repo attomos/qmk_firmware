609ac84bed5c5d2a30b0ecf782bdedbc2334954a
Nattaphoom Chaipreecha <attomos@gmail.com>
2021-11-11 14:46:00 +0700

    feat(cajal): add CTL_ESC tap dance

--- keyboards/walletburner/cajal/keymaps/attomos/keymap.c
+++ keyboards/walletburner/cajal/keymaps/attomos/keymap.c
@@ -37,6 +37,33 @@ enum cajal_keycodes {
   TMUX_M4
 };
 
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+// Tap dance enums
+enum {
+  CTL_ESC
+};
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// For the ctl_esc tap dance. Put it here so it can be used in any keymap
+void ctl_esc_finished(qk_tap_dance_state_t *state, void *user_data);
+void ctl_esc_reset(qk_tap_dance_state_t *state, void *user_data);
 
 #define AA_SLSH LT(_NAV, KC_SLSH)
 
@@ -47,7 +74,7 @@ enum cajal_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_stagger(
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC, RGB_TOG,
-      LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+      TD(CTL_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
       KC_LSFT, AA_Z, AA_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, AA_SLSH, KC_UP,
       KC_LCTL, KC_LGUI, KC_LALT, MO(_ONE), KC_SPC, MO(_TWO), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -326,3 +353,92 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     }
 }
 // }}}
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    }
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is \'KC_W\', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a \'TD_TRIPLE_SINGLE_TAP\', and define that enum just like \'TD_DOUBLE_SINGLE_TAP\'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
+}
+
+// Create an instance of 'td_tap_t' for the 'ctl_esc' tap dance.
+static td_tap_t ctl_esc_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void ctl_esc_finished(qk_tap_dance_state_t *state, void *user_data) {
+    ctl_esc_tap_state.state = cur_dance(state);
+    switch (ctl_esc_tap_state.state) {
+        case TD_UNKNOWN: break;
+        case TD_NONE: break;
+        case TD_TRIPLE_TAP: break;
+        case TD_TRIPLE_HOLD: break;
+        case TD_SINGLE_TAP: register_code(KC_LCTRL); break;
+        case TD_SINGLE_HOLD: register_code(KC_LCTRL); break;
+        case TD_DOUBLE_TAP: register_code(KC_ESC); break;
+        case TD_DOUBLE_HOLD: register_code(KC_LALT); break;
+        case TD_DOUBLE_SINGLE_TAP: register_code(KC_ESC); break;
+    }
+}
+
+void ctl_esc_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (ctl_esc_tap_state.state) {
+        case TD_UNKNOWN: break;
+        case TD_NONE: break;
+        case TD_TRIPLE_TAP: break;
+        case TD_TRIPLE_HOLD: break;
+        case TD_SINGLE_TAP: unregister_code(KC_LCTRL); break;
+        case TD_SINGLE_HOLD: unregister_code(KC_LCTRL); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
+        case TD_DOUBLE_HOLD: unregister_code(KC_LALT);
+        case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_ESC);
+    }
+    ctl_esc_tap_state.state = TD_NONE;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [CTL_ESC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctl_esc_finished, ctl_esc_reset)
+};
