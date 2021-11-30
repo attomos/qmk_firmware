608942bd26d37b31861a63a95b7a8488054750a3
Nattaphoom Chaipreecha <attomos@gmail.com>
2021-11-30 11:33:06 +0700

    feat(mb44): add MB-44 and my keymap :wink:

---
+++ keyboards/mb44/keymaps/attomos/keymap.c
@@ -0,0 +1,290 @@
+/* Copyright 2020 melonbred
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "quantum.h"
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _LAYER1,
+    _LAYER2
+};
+
+enum mb44_keycodes {
+  QWERTY = SAFE_RANGE,
+  ATOM_M1,
+  ATOM_M2,
+  ATOM_M3,
+  ATOM_M4,
+  ATOM_M5,
+  ATOM_M6,
+  ATOM_M7,
+  ATOM_M8,
+  ATOM_M9,
+  ATOM_N0,
+  ATOM_N1,
+  ATOM_S1,
+  ATOM_S2,
+  ATOM_S3,
+  ATOM_S4,
+  TMUX_M1,
+  TMUX_M2,
+  TMUX_M3,
+  TMUX_M4
+};
+
+#define AA_SLSH LT(_LAYER2, KC_SLSH)
+
+
+// My custom Mod-Tap constants
+#define AA_Z MT(MOD_LCTL | MOD_LALT | MOD_LSFT, KC_Z)
+#define AA_X MT(MOD_LCTL | MOD_LALT | MOD_LGUI, KC_X)
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_2u1u_space(
+        KC_TAB,        KC_Q,    KC_W,    KC_E,                KC_R, KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,                KC_F, KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT,       AA_Z,    AA_X,    KC_C,                KC_V, KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  AA_SLSH,   KC_UP,
+        KC_LCTL,   KC_LGUI, KC_LALT, MO(_LAYER1),       KC_SPC, KC_SPC,                   KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    [_LAYER1] = LAYOUT_2u1u_space(
+        KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0, KC_DEL,
+        KC_F13, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,    KC_QUOT, KC_ENT,
+        _______, ATOM_M7, _______,  ATOM_M1, ATOM_M2, _______, ATOM_M6, ATOM_M3, _______, _______,    _______, _______,
+        RESET, _______, _______, XXXXXXX,          _______, _______,                 _______, _______, _______ 
+    ),
+
+    [_LAYER2] = LAYOUT_2u1u_space(
+        _______,   _______,   _______,   _______,   _______,   _______,    _______,   _______,   _______,   _______,  _______,  _______,
+        KC_F13, KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,  KC_F6, XXXXXXX, XXXXXXX, XXXXXXX, ATOM_M8, KC_VOLU,
+        XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+};
+
+// helper/macro functions
+
+void tmux_previous_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("9"));
+}
+
+void tmux_next_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("0"));
+}
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ATOM_M1:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGUP);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGUP);
+      }
+      return false;
+      break;
+    case ATOM_M2:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGDN);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGDN);
+      }
+      return false;
+      break;
+    case ATOM_M3:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        register_code(KC_F12);
+      } else {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_F12);
+      }
+      return false;
+      break;
+    case ATOM_M4:
+      if (record->event.pressed) {
+        SEND_STRING("[t");
+      }
+      return false;
+      break;
+    case ATOM_M5:
+      if (record->event.pressed) {
+        SEND_STRING("]t");
+      }
+      return false;
+      break;
+    case ATOM_M6:
+      if (record->event.pressed) {
+        SEND_STRING("]T");
+      }
+      return false;
+      break;
+    case ATOM_M7:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_4);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_4);
+      }
+      return false;
+      break;
+    case ATOM_M8:
+      if (record->event.pressed) {
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_3);
+      } else {
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_3);
+      }
+      return false;
+      break;
+    case TMUX_M1:
+      if (record->event.pressed) {
+        tmux_previous_session();
+      }
+      return false;
+      break;
+    case TMUX_M2:
+      if (record->event.pressed) {
+        tmux_next_session();
+      }
+      return false;
+      break;
+    case TMUX_M3:
+      // tmux: rename session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT("4"));
+      }
+      return false;
+      break;
+    case TMUX_M4:
+      // tmux: start a new session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT(";"));
+        SEND_STRING("new");
+        register_code(KC_ENT);
+      } else {
+        unregister_code(KC_ENT);
+      }
+      return false;
+      break;
+    case ATOM_N0:
+      if (record->event.pressed) {
+        SEND_STRING("3j");
+      }
+      return false;
+      break;
+    case ATOM_N1:
+      if (record->event.pressed) {
+        SEND_STRING("3k");
+      }
+      return false;
+      break;
+    case ATOM_S1:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_1);
+      }
+      return false;
+      break;
+    case ATOM_S2:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_2);
+      }
+      return false;
+      break;
+    case ATOM_S3:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        register_code(KC_1);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_1);
+      }
+      return false;
+      break;
+    case ATOM_S4:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        register_code(KC_2);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_2);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+
+// leave my CTL_T(KC_ESC) alone {{{
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AA_Z:
+        case AA_X:
+        case AA_SLSH:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AA_Z:
+        case AA_X:
+        case AA_SLSH:
+            return true;
+        default:
+            return false;
+    }
+}
+// }}}
