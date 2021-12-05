b6da9e477140211d8ff79c326971b4982f310a7c
Nattaphoom Chaipreecha <attomos@gmail.com>
2021-12-05 12:15:21 +0700

    feat(mb44): change the location of F13 and DEL key

--- keyboards/mb44/keymaps/attomos/keymap.c
+++ keyboards/mb44/keymaps/attomos/keymap.c
@@ -64,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LAYER1] = LAYOUT_2u1u_space(
-        KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0, KC_DEL,
-        KC_F13, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,    KC_QUOT, KC_ENT,
+        KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0, _______,
+        KC_DEL, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,    KC_QUOT, KC_ENT,
         _______, ATOM_M7, _______,  ATOM_M1, ATOM_M2, _______, ATOM_M6, ATOM_M3, _______, _______,    _______, _______,
         RESET, _______, _______, XXXXXXX,          _______, _______,                 _______, _______, _______ 
     ),
 
     [_LAYER2] = LAYOUT_2u1u_space(
-        _______,   _______,   _______,   _______,   _______,   _______,    _______,   _______,   _______,   _______,  _______,  _______,
+        _______,   _______,   _______,   _______,   _______,   _______,    _______,   _______,   _______,   _______,  _______,  KC_F13,
         KC_F13, KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,  KC_F6, XXXXXXX, XXXXXXX, XXXXXXX, ATOM_M8, KC_VOLU,
         XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
         _______, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
