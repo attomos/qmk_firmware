dcf3f1e23603d52e964c8adc8c054865d62fd337
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-01-22 10:54:13 +0700

    fix: correct multimedia button positions

--- keyboards/mb44/keymaps/attomos/keymap.c
+++ keyboards/mb44/keymaps/attomos/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER2] = LAYOUT_2u1u_space(
         _______,   _______,   ATOM_S1,   ATOM_S2,   _______,   _______,    _______,   _______,   _______,   _______,  _______,  KC_VOLU,
         KC_F13, KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,  KC_F6, KC_F13, XXXXXXX, XXXXXXX, ATOM_M8, KC_MUTE,
-        XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD,
+        XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, KC_VOLD,
         _______, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
