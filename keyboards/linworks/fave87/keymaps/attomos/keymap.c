617b717c540104aa98207a165bfdcf00621e2d63
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-04-04 16:11:33 +0700

    feat(fave87): remap f-keys like smol keebs and format the layout

--- keyboards/linworks/fave87/keymaps/attomos/keymap.c
+++ keyboards/linworks/fave87/keymaps/attomos/keymap.c
@@ -36,20 +36,20 @@ enum fave87_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_all(
-    KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                 KC_PSCR, KC_F13,  KC_MPLY,
-    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSLS,      KC_INS,  KC_HOME, KC_PGUP,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                KC_DEL,  KC_END,  KC_PGDN,
+    KC_ESC,          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                           KC_PSCR, KC_F13,  KC_MPLY,
+    KC_GRV,          KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSLS,      KC_INS,  KC_HOME, KC_PGUP,
+    KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                KC_DEL,  KC_END,  KC_PGDN,
     LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
-    KC_LSFT,  KC_NUBS,  AA_Z,     AA_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   AA_SLSH,  KC_RSFT,  KC_TRNS,                         KC_UP,
-    KC_LGUI,  XXXXXXX,  KC_LALT,                      KC_SPC,                                           KC_RGUI,  XXXXXXX,  XXXXXXX,  MO(_NAV),               KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_LSFT,         KC_NUBS,  AA_Z,     AA_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   AA_SLSH,  KC_RSFT,  KC_TRNS,                         KC_UP,
+    KC_LGUI,         XXXXXXX,  KC_LALT,                      KC_SPC,                                           KC_RGUI,  XXXXXXX,  XXXXXXX,  MO(_NAV),               KC_LEFT, KC_DOWN, KC_RGHT),
 
   [_NAV] = LAYOUT_all(
-    RGB_TOG,            RGB_VAD,  RGB_VAI,  BL_DEC,   BL_INC,   RGB_HUD,  RGB_HUI,  RGB_SAD,  RGB_SAI,  RGB_RMOD, RGB_MOD,  RGB_SPI,  RGB_SPI,                KC_TRNS, RESET, KC_TRNS,
-    KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,      KC_TRNS, ATOM_S2, KC_BRIU,
-    KC_TRNS,  ATOM_M1,  KC_UP,    ATOM_M2,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_MUTE, ATOM_S1, KC_BRID,
-    KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_BSLS,  KC_TRNS,  KC_TRNS,  KC_PIPE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_HOME,  KC_TRNS,  KC_END,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                         KC_VOLU,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,                      KC_TRNS,                                          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_MPRV, KC_VOLD, KC_MNXT),
+    RGB_TOG,         RGB_VAD,  RGB_VAI,  BL_DEC,   BL_INC,   RGB_HUD,  RGB_HUI,  RGB_SAD,  RGB_SAI,  RGB_RMOD, RGB_MOD,  RGB_SPI,  RGB_SPI,                          KC_TRNS, RESET, KC_TRNS,
+    KC_GRV,          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS, ATOM_S2, KC_BRIU,
+    KC_TRNS,         ATOM_M1,  KC_UP,    ATOM_M2,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_MUTE, ATOM_S1, KC_BRID,
+    KC_TRNS,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_PIPE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,         KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                         KC_VOLU,
+    KC_TRNS,         KC_TRNS,  KC_TRNS,                      KC_TRNS,                                          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_MPRV, KC_VOLD, KC_MNXT),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
