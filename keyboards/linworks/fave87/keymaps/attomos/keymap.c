a7033e99e8b2537f2f6224eed4693a2c80fa4411
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-04-04 16:01:40 +0700

    feat(fave87): use ctrl/esc like smol keebs & use grave accent normally

--- keyboards/linworks/fave87/keymaps/attomos/keymap.c
+++ keyboards/linworks/fave87/keymaps/attomos/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_all(
     KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                 KC_PSCR, KC_F13,  KC_MPLY,
-    KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSLS,      KC_INS,  KC_HOME, KC_PGUP,
+    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_BSLS,      KC_INS,  KC_HOME, KC_PGUP,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                KC_DEL,  KC_END,  KC_PGDN,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+    LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
     KC_LSFT,  KC_NUBS,  AA_Z,     AA_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   AA_SLSH,  KC_RSFT,  KC_TRNS,                         KC_UP,
     KC_LGUI,  XXXXXXX,  KC_LALT,                      KC_SPC,                                           KC_RGUI,  XXXXXXX,  XXXXXXX,  MO(_NAV),               KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -100,6 +100,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AA_Z:
         case AA_X:
+        case AA_SLSH:
             return 900;
         default:
             return TAPPING_TERM;
@@ -110,6 +111,7 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AA_Z:
         case AA_X:
+        case AA_SLSH:
             return true;
         default:
             return false;
@@ -120,6 +122,7 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AA_Z:
         case AA_X:
+        case AA_SLSH:
             return true;
         default:
             return false;
