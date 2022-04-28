43a21da6471add52f10303e476b5a7a3f1abd3fc
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-04-28 11:09:10 +0700

    feat(adelie): add my usual keymaps

--- keyboards/cannonkeys/adelie/keymaps/attomos/keymap.c
+++ keyboards/cannonkeys/adelie/keymaps/attomos/keymap.c
@@ -23,38 +23,82 @@
 // entirely and just use numbers.
 enum layer_names {
     _BASE,
-    _FN1,
-    _FN2,
-    _FN3
+    _LAYER1,
+    _LAYER2,
+    _LAYER3
 };
+
+enum adelie_keycodes {
+  QWERTY = SAFE_RANGE,
+  ATOM_M1,
+  ATOM_M2,
+  ATOM_M3,
+  ATOM_M4,
+  ATOM_M5,
+  ATOM_M6,
+  ATOM_M7,
+  ATOM_M8,
+  ATOM_M9,
+  ATOM_N0,
+  ATOM_N1,
+  ATOM_S1,
+  ATOM_S2,
+  ATOM_S3,
+  ATOM_S4,
+  TMUX_M1,
+  TMUX_M2,
+  TMUX_M3,
+  TMUX_M4,
+  ATOM_T1,
+  ATOM_T2,
+  ATOM_T3,
+  ATOM_T4,
+  ATOM_T5,
+  ATOM_T6,
+  ATOM_T7,
+  ATOM_T8,
+  ATOM_T9,
+  ATOM_T0,
+  ATOM_A0,
+  ATOM_A1,
+  ATOM_A2,
+};
+
+#define AA_COMM LT(_LAYER3, KC_COMM)
+#define AA_SLSH LT(_LAYER2, KC_SLSH)
+
+
+// My custom Mod-Tap constants
+#define AA_Z MT(MOD_LCTL | MOD_LALT | MOD_LSFT, KC_Z)
+#define AA_X MT(MOD_LCTL | MOD_LALT | MOD_LGUI, KC_X)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all(
-    KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC, KC_PGUP,
-    KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP, TG(_FN3),
-    KC_LCTL,  KC_LGUI, KC_LALT, MO(_FN1), KC_SPC, KC_RALT, KC_RGUI, MO(_FN2), KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC, KC_PGUP,
+    LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
+    KC_LSFT,  AA_Z,   AA_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    AA_COMM, KC_DOT,  AA_SLSH,  KC_RSFT, KC_UP, TG(_LAYER2),
+    KC_LCTL,  KC_LGUI, KC_LALT, MO(_LAYER1), KC_SPC, KC_RALT, KC_RGUI, MO(_LAYER2), KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
-  [_FN1] = LAYOUT_all(
-    KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS, KC_HOME,
-    RGB_TOG, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_TRNS, KC_END,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOT, KC_TRNS, KC_TRNS, KC_MINS, KC_PLUS, KC_ASTR, KC_TRNS, KC_EQL, KC_INS, KC_PSCR,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_LAYER1] = LAYOUT_all(
+    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______, _______, _______, KC_HOME,
+    KC_DEL, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______, KC_END,
+    _______, ATOM_M7, _______, ATOM_M1, ATOM_M2, _______, ATOM_M6, ATOM_M3, _______, KC_HOME, KC_END, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [_FN2] = LAYOUT_all(
-    KC_POWER, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, KC_VOLU,
-    KC_SLEP, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
-    KC_WAKE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_MUTE,
-    RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_LAYER2] = LAYOUT_all(
+    _______, _______, ATOM_S1, ATOM_S2, _______, _______, _______, _______, _______, _______, ATOM_M8, _______, _______, _______, KC_VOLU,
+    KC_F13, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, _______, _______, _______, _______, _______, _______, KC_VOLD,
+    KC_SLEP, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, KC_PSCR, KC_MUTE,
+    RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [_FN3] = LAYOUT_all(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1), KC_TRNS, KC_TRNS, KC_TRNS
+  [_LAYER3] = LAYOUT_all(
+    _______, ATOM_T1, ATOM_T2, ATOM_T3, ATOM_T4, ATOM_T5, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, ATOM_T6, ATOM_T7, ATOM_T8, ATOM_T9, ATOM_T0, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, ATOM_A0, ATOM_A1, ATOM_A2, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
@@ -89,8 +133,277 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, _FN1));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _FN2));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _FN3));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _LAYER3));
     return state;
 }
+
+
+// helper/macro functions
+
+void tmux_previous_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("9"));
+}
+
+void tmux_next_session(void) {
+  SEND_STRING(SS_LCTL("a"));
+  SEND_STRING(SS_LSFT("0"));
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ATOM_M1:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGUP);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGUP);
+      }
+      return false;
+      break;
+    case ATOM_M2:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_PGDN);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_PGDN);
+      }
+      return false;
+      break;
+    case ATOM_M3:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        register_code(KC_F12);
+      } else {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_F12);
+      }
+      return false;
+      break;
+    case ATOM_M4:
+      if (record->event.pressed) {
+        SEND_STRING("[t");
+      }
+      return false;
+      break;
+    case ATOM_M5:
+      if (record->event.pressed) {
+        SEND_STRING("]t");
+      }
+      return false;
+      break;
+    case ATOM_M6:
+      if (record->event.pressed) {
+        SEND_STRING("]T");
+      }
+      return false;
+      break;
+    case ATOM_M7:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_4);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_4);
+      }
+      return false;
+      break;
+    case ATOM_M8:
+      if (record->event.pressed) {
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        register_code(KC_3);
+      } else {
+        unregister_code(KC_LALT);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_3);
+      }
+      return false;
+      break;
+    case TMUX_M1:
+      if (record->event.pressed) {
+        tmux_previous_session();
+      }
+      return false;
+      break;
+    case TMUX_M2:
+      if (record->event.pressed) {
+        tmux_next_session();
+      }
+      return false;
+      break;
+    case TMUX_M3:
+      // tmux: rename session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT("4"));
+      }
+      return false;
+      break;
+    case TMUX_M4:
+      // tmux: start a new session
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTL("a"));
+        SEND_STRING(SS_LSFT(";"));
+        SEND_STRING("new");
+        register_code(KC_ENT);
+      } else {
+        unregister_code(KC_ENT);
+      }
+      return false;
+      break;
+    case ATOM_N0:
+      if (record->event.pressed) {
+        SEND_STRING("3j");
+      }
+      return false;
+      break;
+    case ATOM_N1:
+      if (record->event.pressed) {
+        SEND_STRING("3k");
+      }
+      return false;
+      break;
+    case ATOM_S1:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_1);
+      }
+      return false;
+      break;
+    case ATOM_S2:
+      if (record->event.pressed) {
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_SCROLLLOCK);
+        tap_code(KC_2);
+      }
+      return false;
+      break;
+    case ATOM_S3:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        register_code(KC_1);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_1);
+      }
+      return false;
+      break;
+    case ATOM_S4:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+        register_code(KC_LALT);
+        register_code(KC_LGUI);
+        register_code(KC_LSFT);
+        register_code(KC_2);
+      } else {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_2);
+      }
+      return false;
+      break;
+    case ATOM_T1:
+      SEND_STRING(SS_RGUI("1"));
+      return false;
+      break;
+    case ATOM_T2:
+      SEND_STRING(SS_RGUI("2"));
+      return false;
+      break;
+    case ATOM_T3:
+      SEND_STRING(SS_RGUI("3"));
+      return false;
+      break;
+    case ATOM_T4:
+      SEND_STRING(SS_RGUI("4"));
+      return false;
+      break;
+    case ATOM_T5:
+      SEND_STRING(SS_RGUI("5"));
+      return false;
+      break;
+    case ATOM_T6:
+      SEND_STRING(SS_RGUI("6"));
+      return false;
+      break;
+    case ATOM_T7:
+      SEND_STRING(SS_RGUI("7"));
+      return false;
+      break;
+    case ATOM_T8:
+      SEND_STRING(SS_RGUI("8"));
+      return false;
+      break;
+    case ATOM_T9:
+      SEND_STRING(SS_RGUI("9"));
+      return false;
+      break;
+    case ATOM_T0:
+      SEND_STRING(SS_RGUI("0"));
+      return false;
+      break;
+    case ATOM_A0:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RGUI("-"));
+      }
+      return false;
+      break;
+    case ATOM_A1:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RGUI(SS_LSFT("=")));
+      }
+      return false;
+      break;
+    case ATOM_A2:
+      if (record->event.pressed) {
+        SEND_STRING(SS_RGUI("0"));
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+
+// leave my CTL_T(KC_ESC) alone {{{
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AA_Z:
+        case AA_X:
+        case AA_SLSH:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case AA_Z:
+        case AA_X:
+        case AA_SLSH:
+            return true;
+        default:
+            return false;
+    }
+}
+// }}}
