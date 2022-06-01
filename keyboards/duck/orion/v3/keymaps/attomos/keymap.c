2893fc28a2b9eedf8786914d934886cd2d546aea
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-06-01 23:26:22 +0700

    feat(duck): home row mods (4 keys) on a TKL board???

--- keyboards/duck/orion/v3/keymaps/attomos/keymap.c
+++ keyboards/duck/orion/v3/keymaps/attomos/keymap.c
@@ -30,6 +30,9 @@ enum layer_names {
 #define AA_Z MT(MOD_LCTL | MOD_LALT | MOD_LSFT, KC_Z)
 #define AA_X MT(MOD_LCTL | MOD_LALT | MOD_LGUI, KC_X)
 
+#define AA_S RALT_T(KC_S)
+#define AA_D RGUI_T(KC_D)
+
 #define AA_K RGUI_T(KC_K)
 #define AA_L RALT_T(KC_L)
 
@@ -38,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,           KC_PSCR,KC_SLCK,KC_PAUS,
   KC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,KC_MINS, KC_EQL,KC_BSPC,   KC_INS ,KC_HOME,KC_PGUP,
   KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_LBRC,KC_RBRC,KC_BSLS,   KC_DEL ,KC_END ,KC_PGDN,
-  LCTL_T(KC_ESC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   AA_K,   AA_L,KC_SCLN,KC_QUOT,         KC_ENT,
+  LCTL_T(KC_ESC),   KC_A,   AA_S,   AA_D,   KC_F,   KC_G,   KC_H,   KC_J,   AA_K,   AA_L,KC_SCLN,KC_QUOT,         KC_ENT,
   KC_LSFT,    AA_Z,   AA_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,        KC_RSFT,            KC_UP,
   KC_LGUI,XXXXXXX,KC_LALT,                 KC_SPC,                                KC_RALT,KC_RGUI, XXXXXXX, MO(_LAYER1),  KC_LEFT,KC_DOWN,KC_RGHT),
 
@@ -57,6 +60,8 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AA_Z:
         case AA_X:
+        case AA_S:
+        case AA_D:
         case AA_K:
         case AA_L:
         case AA_SLSH:
@@ -70,6 +75,8 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case AA_Z:
         case AA_X:
+        case AA_S:
+        case AA_D:
         case AA_K:
         case AA_L:
         case AA_SLSH:
